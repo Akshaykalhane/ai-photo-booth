3052546b79c1d734372a192590954aad6fea6205
pranjalgogoi1209 <gogoipranjal2022@gmail.com>
2025-02-04 15:14:21 +0530

    update

--- src/components/images/images.js
+++ src/components/images/images.js
@@ -1,235 +1,229 @@
-import image01 from '/superHero/superHeroCards/photoboothCard-07.jpg'
-import image02 from '/superHero/superHeroCards/photoboothCard-08.jpg'
-import image03 from '/superHero/superHeroCards/photoboothCard-09.jpg'
-import image04 from '/superHero/superHeroCards/photoboothCard-10.jpg'
-import image05 from '/superHero/superHeroCards/photoboothCard-11.jpg'
-import image06 from '/hairStyle/hairStyleCards/photoboothCard-13.jpg'
-import image07 from '/hairStyle/hairStyleCards/photoboothCard-14.jpg'
-import image08 from '/hairStyle/hairStyleCards/photoboothCard-15.jpg'
-import image09 from '/hairStyle/hairStyleCards/photoboothCard-16.jpg'
-import image10 from '/hairStyle/hairStyleCards/photoboothCard-17.jpg'
+import image01 from "/superHero/superHeroCards/photoboothCard-07.jpg";
+import image02 from "/superHero/superHeroCards/photoboothCard-08.jpg";
+import image03 from "/superHero/superHeroCards/photoboothCard-09.jpg";
+import image04 from "/superHero/superHeroCards/photoboothCard-10.jpg";
+import image05 from "/superHero/superHeroCards/photoboothCard-11.jpg";
+import image06 from "/hairStyle/hairStyleCards/photoboothCard-13.jpg";
+import image07 from "/hairStyle/hairStyleCards/photoboothCard-14.jpg";
+import image08 from "/hairStyle/hairStyleCards/photoboothCard-15.jpg";
+import image09 from "/hairStyle/hairStyleCards/photoboothCard-16.jpg";
+import image10 from "/hairStyle/hairStyleCards/photoboothCard-17.jpg";
 
 //professional
-import image11 from '/professional/professionalCards/photoboothCard-01.jpg'
-import image12 from '/professional/professionalCards/photoboothCard-02.jpg'
-import image13 from '/professional/professionalCards/photoboothCard-03.jpg'
-import image14 from '/professional/professionalCards/photoboothCard-04.jpg'
-import image15 from '/professional/professionalCards/photoboothCard-05.jpg'
-import image16 from '/professional/professionalCards/photoboothCard-06.jpg'
+import image11 from "/professional/professionalCards/photoboothCard-01.jpg";
+import image12 from "/professional/professionalCards/photoboothCard-02.jpg";
+import image13 from "/professional/professionalCards/photoboothCard-03.jpg";
+import image14 from "/professional/professionalCards/photoboothCard-04.jpg";
+import image15 from "/professional/professionalCards/photoboothCard-05.jpg";
+import image16 from "/professional/professionalCards/photoboothCard-06.jpg";
 
 //original
-import originalImage13 from '/professional/professionalOriginalImages/photoboothOriginalImage-01.jpg';
-import originalImage14 from '/professional/professionalOriginalImages/photoboothOriginalImage-02.jpg';
-import originalImage15 from '/professional/professionalOriginalImages/photoboothOriginalImage-03.jpg';
-import originalImage16 from '/professional/professionalOriginalImages/photoboothOriginalImage-04.jpg';
-import originalImage17 from '/professional/professionalOriginalImages/photoboothOriginalImage-05.jpg';
-import originalImage18 from '/professional/professionalOriginalImages/photoboothOriginalImage-06.jpg';
+import originalImage13 from "/professional/professionalOriginalImages/photoboothOriginalImage-01.jpg";
+import originalImage14 from "/professional/professionalOriginalImages/photoboothOriginalImage-02.jpg";
+import originalImage15 from "/professional/professionalOriginalImages/photoboothOriginalImage-03.jpg";
+import originalImage16 from "/professional/professionalOriginalImages/photoboothOriginalImage-04.jpg";
+import originalImage17 from "/professional/professionalOriginalImages/photoboothOriginalImage-05.jpg";
+import originalImage18 from "/professional/professionalOriginalImages/photoboothOriginalImage-06.jpg";
 
 //caricature
-import image17 from '/caricature/caricatureCards/photoboothCard-19.jpg'
-import image18 from '/caricature/caricatureCards/photoboothCard-20.jpg'
-import image19 from '/caricature/caricatureCards/photoboothCard-21.jpg'
-import image20 from '/caricature/caricatureCards/photoboothCard-22.jpg'
-import image21 from '/caricature/caricatureCards/photoboothCard-23.jpg'
-import image22 from '/caricature/caricatureCards/photoboothCard-24.jpg'
+import image17 from "/caricature/caricatureCards/photoboothCard-19.jpg";
+import image18 from "/caricature/caricatureCards/photoboothCard-20.jpg";
+import image19 from "/caricature/caricatureCards/photoboothCard-21.jpg";
+import image20 from "/caricature/caricatureCards/photoboothCard-22.jpg";
+import image21 from "/caricature/caricatureCards/photoboothCard-23.jpg";
+import image22 from "/caricature/caricatureCards/photoboothCard-24.jpg";
 
 //original
-import originalImage19 from '/caricature/caricatureOriginalImages/photoboothOriginalImage-19.jpg';
-import originalImage20 from '/caricature/caricatureOriginalImages/photoboothOriginalImage-20.jpg';
-import originalImage21 from '/caricature/caricatureOriginalImages/photoboothOriginalImage-21.jpg';
-import originalImage22 from '/caricature/caricatureOriginalImages/photoboothOriginalImage-22.jpg';
-import originalImage23 from '/caricature/caricatureOriginalImages/photoboothOriginalImage-23.jpg';
-import originalImage24 from '/caricature/caricatureOriginalImages/photoboothOriginalImage-24.jpg';
+import originalImage19 from "/caricature/caricatureOriginalImages/photoboothOriginalImage-19.jpg";
+import originalImage20 from "/caricature/caricatureOriginalImages/photoboothOriginalImage-20.jpg";
+import originalImage21 from "/caricature/caricatureOriginalImages/photoboothOriginalImage-21.jpg";
+import originalImage22 from "/caricature/caricatureOriginalImages/photoboothOriginalImage-22.jpg";
+import originalImage23 from "/caricature/caricatureOriginalImages/photoboothOriginalImage-23.jpg";
+import originalImage24 from "/caricature/caricatureOriginalImages/photoboothOriginalImage-24.jpg";
 
-import originalImage01 from '/superHero/superHeroOriginalImages/photoboothOriginalImage-07.jpg';
-import originalImage02 from '/superHero/superHeroOriginalImages/photoboothOriginalImage-08.jpg';
-import originalImage03 from '/superHero/superHeroOriginalImages/photoboothOriginalImage-09.jpg';
-import originalImage04 from '/superHero/superHeroOriginalImages/photoboothOriginalImage-10.jpg';
-import originalImage05 from '/superHero/superHeroOriginalImages/photoboothOriginalImage-11.jpg';
-import originalImage06 from '/superHero/superHeroOriginalImages/photoboothOriginalImage-12.jpg';
-import image_06  from '/superHero/superHeroCards/photoboothCard-12.jpg'
+import originalImage01 from "/superHero/superHeroOriginalImages/photoboothOriginalImage-07.jpg";
+import originalImage02 from "/superHero/superHeroOriginalImages/photoboothOriginalImage-08.jpg";
+import originalImage03 from "/superHero/superHeroOriginalImages/photoboothOriginalImage-09.jpg";
+import originalImage04 from "/superHero/superHeroOriginalImages/photoboothOriginalImage-10.jpg";
+import originalImage05 from "/superHero/superHeroOriginalImages/photoboothOriginalImage-11.jpg";
+import originalImage06 from "/superHero/superHeroOriginalImages/photoboothOriginalImage-12.jpg";
+import image_06 from "/superHero/superHeroCards/photoboothCard-12.jpg";
 
-import originalImage07 from '/hairStyle/hairStyleOriginalImages/photoboothOriginalImage-13.jpg';
-import originalImage08 from '/hairStyle/hairStyleOriginalImages/photoboothOriginalImage-14.jpg';
-import originalImage09 from '/hairStyle/hairStyleOriginalImages/photoboothOriginalImage-15.jpg';
-import originalImage10 from '/hairStyle/hairStyleOriginalImages/photoboothOriginalImage-16.jpg';
-import originalImage11 from '/hairStyle/hairStyleOriginalImages/photoboothOriginalImage-17.jpg';
-import originalImage12 from '/hairStyle/hairStyleOriginalImages/photoboothOriginalImage-18.jpg';
-import image_07 from '/hairStyle/hairStyleCards/photoboothCard-18.jpg'
-
-
-
+import originalImage07 from "/hairStyle/hairStyleOriginalImages/photoboothOriginalImage-13.jpg";
+import originalImage08 from "/hairStyle/hairStyleOriginalImages/photoboothOriginalImage-14.jpg";
+import originalImage09 from "/hairStyle/hairStyleOriginalImages/photoboothOriginalImage-15.jpg";
+import originalImage10 from "/hairStyle/hairStyleOriginalImages/photoboothOriginalImage-16.jpg";
+import originalImage11 from "/hairStyle/hairStyleOriginalImages/photoboothOriginalImage-17.jpg";
+import originalImage12 from "/hairStyle/hairStyleOriginalImages/photoboothOriginalImage-18.jpg";
+import image_07 from "/hairStyle/hairStyleCards/photoboothCard-18.jpg";
 
 export const images = [
-    image01,
-    image02,
-    image03,
-    image04,
-    image05,
-    image06,
-    image07,
-    image08,
-    image09,
-    image10,
-    image11,
-    image12
+  image01,
+  image02,
+  image03,
+  image04,
+  image05,
+  image06,
+  image07,
+  image08,
+  image09,
+  image10,
+  image11,
+  image12,
 ];
 
-
-
-export const superHeroImage=[
-    {
-        id:1,
-        imageSrc:image01,
-        originalSrc:originalImage01,
-        gender:'male',
-    },
-    {
-        id:2,
-        imageSrc:image02,
-        originalSrc:originalImage02,
-        gender:'male'
-    },
-    {
-        id:3,
-        imageSrc:image03,
-        originalSrc:originalImage03,
-        gender:'male'
-    },
-    {
-        id:4,
-        imageSrc:image04,
-        originalSrc:originalImage04,
-        gender:'female'
-    },
-    {
-        id:5,
-        imageSrc:image05,
-        originalSrc:originalImage05,
-        gender:'female'
-    },
-    {
-        id:6,
-        imageSrc:image_06,
-        originalSrc:originalImage06,
-        gender:'female'
-    },
+export const superHeroImage = [
+  {
+    id: 1,
+    imageSrc: image01,
+    originalSrc: originalImage01,
+    gender: "male",
+  },
+  {
+    id: 2,
+    imageSrc: image02,
+    originalSrc: originalImage02,
+    gender: "male",
+  },
+  {
+    id: 3,
+    imageSrc: image03,
+    originalSrc: originalImage03,
+    gender: "male",
+  },
+  {
+    id: 4,
+    imageSrc: image04,
+    originalSrc: originalImage04,
+    gender: "female",
+  },
+  {
+    id: 5,
+    imageSrc: image05,
+    originalSrc: originalImage05,
+    gender: "female",
+  },
+  {
+    id: 6,
+    imageSrc: image_06,
+    originalSrc: originalImage06,
+    gender: "female",
+  },
 ];
 
-export const hairStyle=[
-    {
-        id:7,
-        imageSrc:image06,
-        originalSrc:originalImage07,
-        gender:'male'
-    },
-    {
-        id:8,
-        imageSrc:image07,
-        originalSrc:originalImage08,
-        gender:'male'
-    },
-    {
-        id:9,
-        imageSrc:image08,
-        originalSrc:originalImage09,
-        gender:'male'
-    },
-    {
-        id:10,
-        imageSrc:image09,
-        originalSrc:originalImage10,
-        gender:'female'
-    },
-    {
-        id:11,
-        imageSrc:image10,
-        originalSrc:originalImage11,
-        gender:'female'
-    },
-    {
-        id:12,
-        imageSrc:image_07,
-        originalSrc:originalImage12,
-        gender:'female'
-    },
-]
-
+export const hairStyle = [
+  {
+    id: 7,
+    imageSrc: image06,
+    originalSrc: originalImage07,
+    gender: "male",
+  },
+  {
+    id: 8,
+    imageSrc: image07,
+    originalSrc: originalImage08,
+    gender: "male",
+  },
+  {
+    id: 9,
+    imageSrc: image08,
+    originalSrc: originalImage09,
+    gender: "male",
+  },
+  {
+    id: 10,
+    imageSrc: image09,
+    originalSrc: originalImage10,
+    gender: "female",
+  },
+  {
+    id: 11,
+    imageSrc: image10,
+    originalSrc: originalImage11,
+    gender: "female",
+  },
+  {
+    id: 12,
+    imageSrc: image_07,
+    originalSrc: originalImage12,
+    gender: "female",
+  },
+];
 
 export const professional = [
-    {
-        id:13,
-        imageSrc:image11,
-        originalSrc:originalImage13,
-        gender:'female'
-    },
-    {
-        id:14,
-        imageSrc:image12,
-        originalSrc:originalImage14,
-        gender:'female'
-    },
-    {
-        id:15,
-        imageSrc:image13,
-        originalSrc:originalImage15,
-        gender:'female'
-    },
-    {
-        id:16,
-        imageSrc:image14,
-        originalSrc:originalImage16,
-        gender:'male'
-    },
-    {
-        id:17,
-        imageSrc:image15,
-        originalSrc:originalImage17,
-        gender:'male'
-    },
-    {
-        id:18,
-        imageSrc:image16,
-        originalSrc:originalImage18,
-        gender:'male'
-    },
-]
+  {
+    id: 13,
+    imageSrc: image11,
+    originalSrc: originalImage13,
+    gender: "female",
+  },
+  {
+    id: 14,
+    imageSrc: image12,
+    originalSrc: originalImage14,
+    gender: "female",
+  },
+  {
+    id: 15,
+    imageSrc: image13,
+    originalSrc: originalImage15,
+    gender: "female",
+  },
+  {
+    id: 16,
+    imageSrc: image14,
+    originalSrc: originalImage16,
+    gender: "male",
+  },
+  {
+    id: 17,
+    imageSrc: image15,
+    originalSrc: originalImage17,
+    gender: "male",
+  },
+  {
+    id: 18,
+    imageSrc: image16,
+    originalSrc: originalImage18,
+    gender: "male",
+  },
+];
 
 export const caricature = [
-    {
-        id:19,
-        imageSrc:image17,
-        originalSrc:originalImage19,
-        gender:'female'
-    },
-    {
-        id:20,
-        imageSrc:image18,
-        originalSrc:originalImage20,
-        gender:'female'
-    },
-    {
-        id:21,
-        imageSrc:image19,
-        originalSrc:originalImage21,
-        gender:'female'
-    },
-    {
-        id:22,
-        imageSrc:image20,
-        originalSrc:originalImage22,
-        gender:'male'
-    },
-    {
-        id:23,
-        imageSrc:image21,
-        originalSrc:originalImage23,
-        gender:'male'
-    },
-    {
-        id:24,
-        imageSrc:image22,
-        originalSrc:originalImage24,
-        gender:'male'
-    },
-]
+  {
+    id: 19,
+    imageSrc: image17,
+    originalSrc: originalImage19,
+    gender: "female",
+  },
+  {
+    id: 20,
+    imageSrc: image18,
+    originalSrc: originalImage20,
+    gender: "male",
+  },
+  {
+    id: 21,
+    imageSrc: image19,
+    originalSrc: originalImage21,
+    gender: "female",
+  },
+  {
+    id: 22,
+    imageSrc: image20,
+    originalSrc: originalImage22,
+    gender: "female",
+  },
+  {
+    id: 23,
+    imageSrc: image21,
+    originalSrc: originalImage23,
+    gender: "male",
+  },
+  {
+    id: 24,
+    imageSrc: image22,
+    originalSrc: originalImage24,
+    gender: "male",
+  },
+];
